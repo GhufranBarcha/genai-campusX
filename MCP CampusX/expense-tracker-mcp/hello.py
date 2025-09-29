8ee6e02e510259c2e11819b1177e164eb4e6d0be
GhufranBarcha <gufoor.barcha2906@gmail.com>
2025-09-30 02:10:29 +0500

    Expense Tracker version 1

--- MCP CampusX/expense-tracker-mcp/hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from expense-tracker-mcp!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ MCP CampusX/expense-tracker-mcp/main.py
@@ -0,0 +1,47 @@
+import os
+from fastmcp import FastMCP
+import sqlite3
+
+
+DB_PATH = os.path.join(os.path.dirname(__file__), "expense.db")
+
+## Create a FastMCP server instance
+mcp = FastMCP(name="MCP Expense Tracker")
+
+
+def init_db():
+    with sqlite3.connect(DB_PATH) as c:
+        c.execute(
+            """
+            CREATE TABLE IF NOT EXISTS expenses(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT NOT NULL,
+            amount REAL NOT NULL,
+            category TEXT NOT NULL,
+            subcategory TEXT DEFAULT '',
+            note TEXT DEFAULT ''
+            )
+        """
+        )
+init_db()
+
+@mcp.tool
+def add_expense(date, amount, category, subcategory="", note=""):
+    """Add a new expense entry to the database."""
+    with sqlite3.connect(DB_PATH) as c:
+        curr =  c.execute("INSERT INTO expenses(date, amount, category, subcategory, note) VALUES (?,?,?,?,?)",
+                          (date, amount, category, subcategory, note))
+        return {"status": "ok", "id": curr.lastrowid}
+
+@mcp.tool
+def list_expense():
+    """List all expense entries from the database."""
+    with sqlite3.connect(DB_PATH) as c:
+        curr =  c.execute("SELECT id, date, amount, category, subcategory, note FROM expenses ORDER BY id ASC")
+        cols = [d[0] for d in curr.description]
+        return [dict(zip(cols, r)) for r in curr.fetchall()]
+      
+
+
+if __name__ == "__main__":
+    mcp.run()
